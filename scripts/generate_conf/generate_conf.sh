c67a1f4e811c06655b89855dbf29f1f8493adcf3
sahilng <sahilng1997@gmail.com>
2024-11-11 10:50:20 -0500

    updates

--- scripts/generate_conf/generate_conf.sh
+++ scripts/generate_conf/generate_conf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if venv exists; if not, create it and install dependencies
-if [ ! -d "./scripts/generate_conf_py/venv" ]; then
+if [ ! -d "./scripts/generate_conf/venv" ]; then
     echo "Virtual environment not found. Creating it..."
-    python3 -m venv ./scripts/generate_conf_py/venv
+    python3 -m venv ./scripts/generate_conf/venv
 fi
 
 echo "Installing dependencies..."
-./scripts/generate_conf_py/venv/bin/python -m pip install -r ./scripts/generate_conf_py/requirements.txt
+./scripts/generate_conf/venv/bin/python -m pip install -r ./scripts/generate_conf/requirements.txt
 
 # Run the Python script using the virtual environment's Python interpreter
-./scripts/generate_conf_py/venv/bin/python ./scripts/generate_conf_py/generate_conf.py
+./scripts/generate_conf/venv/bin/python ./scripts/generate_conf/generate_conf.py
