b95485d26b8b0694828aa5f9851f2fdf50b7df41
sahilng <sahilng1997@gmail.com>
2024-11-11 04:27:51 -0500

    update

--- scripts/generate_conf.sh
+++ scripts/generate_conf.sh
@@ -7,7 +7,7 @@ if [ ! -d "./scripts/generate_conf_py/venv" ]; then
 fi
 
 echo "Installing dependencies..."
-./scripts/generate_conf_py/venv/bin/pip install -r ./scripts/generate_conf_py/requirements.txt
+./scripts/generate_conf_py/venv/bin/python -m pip install -r ./scripts/generate_conf_py/requirements.txt
 
 # Run the Python script using the virtual environment's Python interpreter
 ./scripts/generate_conf_py/venv/bin/python ./scripts/generate_conf_py/generate_conf.py
